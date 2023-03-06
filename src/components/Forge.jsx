d81d87a6125bd75c8a6340a458de8bd8c11978c1
mattwinans <m@matt.mw>
2023-03-06 14:05:11 -0600

    Forge.jsx & mohAbi.json - Provider integration & wallet connectivity beta 1

--- src/components/Forge.jsx
+++ src/components/Forge.jsx
@@ -9,13 +9,36 @@ import uncommonVideo from "../videos/uncommon_video.mp4";
 import rareVideo from "../videos/rare_video.mp4";
 import epicVideo from "../videos/epic_video.mp4";
 import legendaryVideo from "../videos/legendary_video.mp4";
+import detectEthereumProvider from '@metamask/detect-provider';
 
+// const BinanceChain = {
+//   name: "BSC Mainnet",
+//   rpcURL: ["https://bsc-dataseed.binance.org/"],
+//   chainId: "0x38",
+//   nativeCurrency: {
+//     name: 'BNB',
+//     symbol: 'BNB',
+//     decimals: 18,
+//   },
+//   blockExplorerUrls: ['https://bscscan.com'],
+// }
+
+const BinanceChain = {
+  name: "BSC Testnet",
+  rpcURL: ["https://data-seed-prebsc-1-s1.binance.org:8545/"],
+  chainId: '0x61',
+  nativeCurrency: {
+    name: 'BNB',
+    symbol: 'BNB',
+    decimals: 18,
+  },
+  blockExplorerUrls: ['https://testnet.bscscan.com'],
+}
 
 const Forge = () => {
- const contractAddress = '0xa7f455976455F328D6E73D345E21B319bDe6A7b3';
- const web3 = new Web3(window.BinanceChain);
- //const medalsOfHonor = new web3.eth.Contract(contractAbi, contractAddress);
- const ipfsHash = '/ipfs/QmTjjf1DRjdRWvuSsh5LGXVUi2AKTiTXbcH4a2oVxbouog/'; 
+  const contractAddress = '0xa7f455976455F328D6E73D345E21B319bDe6A7b3';
+  //const medalsOfHonor = new web3.eth.Contract(contractAbi, contractAddress);
+  //  const ipfsHash = '/ipfs/QmTjjf1DRjdRWvuSsh5LGXVUi2AKTiTXbcH4a2oVxbouog/'; 
 
   const [isOpen, setOpen] = useState(false);
   const [videoId, setVideoId] = useState('');
@@ -26,6 +49,7 @@ const Forge = () => {
       name: 'COMMON',
       creator: 'XDRIP OFFICIAL',
       price: '0.25 BNB',
+      priceNumber: 0.25,
       nft: commonVideo,
       videoId: 'nWHNzR660TU',
       ipfsHash: '/ipfs/QmRmB8XKF39SPN6EPD9uqB5PbYZuQb3ZCbGyBvkp3iPapX?filename=COMMON.mp4'
@@ -35,6 +59,7 @@ const Forge = () => {
       name: 'UNCOMMON',
       creator: 'XDRIP OFFICIAL',
       price: '0.50 BNB',
+      priceNumber: 0.5,
       nft: uncommonVideo,
       videoId: '7w94uyo24g0',
       ipfsHash: '/ipfs/QmejQNntRF55PdeJ8o3Kf5Yt7gYdibdQsbmzHYFY5FwfDx?filename=UNCOMMON.mp4'
@@ -44,6 +69,7 @@ const Forge = () => {
       name: 'RARE',
       creator: 'XDRIP OFFICIAL',
       price: '0.75 BNB',
+      priceNumber: 0.75,
       nft: rareVideo,
       videoId: 'jQG6tgMtLbk',
       ipfsHash: '/ipfs/QmVZQWfTpBJQKtAhdxE85uF3chUGF9aTFE2ZjAoRj2wKG7?filename=RARE.mp4'
@@ -53,6 +79,7 @@ const Forge = () => {
       name: 'EPIC',
       creator: 'XDRIP OFFICIAL',
       price: '1.0 BNB',
+      priceNumber: 1,
       nft: epicVideo,
       videoId: 't7psdW_7fZI',
       ipfsHash: '/ipfs/QmcpkEPjttpwBoZPiuCyDtqj8WHpqSVfZv6xa2TRfR7rEU?filename=EPIC.mp4'
@@ -62,6 +89,7 @@ const Forge = () => {
       name: 'LEGENDARY',
       creator: 'XDRIP OFFICIAL',
       price: '1.5 BNB',
+      priceNumber: 1.5,
       nft: legendaryVideo,
       videoId: 'e_Yr4s7fTTA',
       ipfsHash: '/ipfs/QmfNX1afjGa4WwYUZrv1SUhp5DkVBqZ9sPUkcSWYySfovx?filename=LEGENDARY.mp4'
@@ -71,42 +99,84 @@ const Forge = () => {
   const handleMint = async (nftId, nftName) => {
     // find the NFT with the matching ID
     const selectedNft = nfts.find(nft => nft.id === nftId);
-  
+
     // make sure the selected NFT exists
     if (!selectedNft) {
       console.error(`NFT with ID ${nftId} not found`);
       return;
     }
-  
+
     // Confirm the purchase
     const confirmed = window.confirm(`FORGE YOUR ${selectedNft.name} MEDAL FOR ${selectedNft.price}?`);
-  
+
     if (!confirmed) {
       return;
     }
-  
+
+    const provider = await detectEthereumProvider();
+    try {
+      const curChainId = await window.ethereum.request({ method: 'eth_chainId' });
+      if (curChainId != BinanceChain.chainId) {
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [
+            {
+              chainId: BinanceChain.chainId,
+            },
+          ],
+        })
+      }
+    } catch (error) {
+      if (error.code === 4902) {
+        try {
+          await window.ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: BinanceChain.chainId,
+                chainName: BinanceChain.name,
+                nativeCurrency: BinanceChain.nativeCurrency,
+                rpcUrls: BinanceChain.rpcURL,
+                blockExplorerUrls: BinanceChain.blockExplorerUrls
+              },
+            ],
+          });
+          return true
+        } catch (adderror) {
+          console.error('Failed to add the network in Metamask:', adderror)
+          return false
+        }
+      } else {
+        console.error('Failed to setup the network in Metamask:', error)
+        return
+      }
+    }
+
     // call the smart contract function to mint the NFT
     try {
       // Prompt user to connect MetaMask
       await window.ethereum.enable();
-  
+      const web3 = new Web3(provider);
+
       // Get user's BSC address
       const accounts = await web3.eth.getAccounts();
       const userAddress = accounts[0];
-  
+
       // Get the contract instance
       const contract = new web3.eth.Contract(contractAbi, contractAddress);
-  
+
       // Mint the NFT
-      const mintTx = await contract.methods.mintNFT(userAddress, ipfsHash, selectedNft.name).send({ from: userAddress, value: web3.utils.toWei(selectedNft.price, 'ether') });
-  
+      const mintTx = await contract.methods.mintNFT(selectedNft.name, selectedNft.ipfsHash).send({
+        from: userAddress,
+        value: web3.utils.toWei(selectedNft.priceNumber.toString(), 'ether')
+      });
+
       console.log(mintTx);
       console.log(`Minted NFT: ${selectedNft.name} (${nftName})`);
     } catch (error) {
       console.error(error);
     }
   };
-  
 
   return (
     <>
@@ -118,14 +188,14 @@ const Forge = () => {
                 <video className="w-full h-full object-cover" title="Click to preview NFT" src={nft.nft} autoPlay loop muted onClick={() => { setOpen(true); setVideoId(nft.videoId); }} />
               </div>
               <div className="p-4">
-                <h4 className="mt-0 mb-0 text-2xl text-white">{nft.name}<br/>MEDAL</h4>
+                <h4 className="mt-0 mb-0 text-2xl text-white">{nft.name}<br />MEDAL</h4>
                 <p className="text-sm text-white mb-0">Presented By</p>
-                <h3 className="text-sm text-white mb-12">{nft.creator}</h3>              
+                <h3 className="text-sm text-white mb-12">{nft.creator}</h3>
                 <p className="mt- text-md text-white">{nft.price}</p>
-                <button 
+                <button
                   className="mt-4 w-full inline-flex justify-center border px-4 py-2 
                     gradient-bg-button text-base text-black hover:from-blue-400 hover:to-purple-400 focus:outline-none 
-                    focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" 
+                    focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                   onClick={() => handleMint(nft.id, nft.name)}
                 >
                   FORGE YOUR MEDAL
