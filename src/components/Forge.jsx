47e593c1d64edd6535e224b638b1ffe7f2230ea1
XdRiP <109847848+XdRiPOfficial@users.noreply.github.com>
2023-03-07 19:22:49 -0700

    Revert

--- src/components/Forge.jsx
+++ src/components/Forge.jsx
@@ -9,46 +9,52 @@ import uncommonVideo from "../videos/uncommon_video.mp4";
 import rareVideo from "../videos/rare_video.mp4";
 import epicVideo from "../videos/epic_video.mp4";
 import legendaryVideo from "../videos/legendary_video.mp4";
-import { Web3ReactProvider, useWeb3React } from '@web3-react/core';
-
 
 const mohContractABI = contractABI;
-const mohContractAddress = '0xaF0B5f4d9F777fb4CAC449ff6F759e4979d184d4'; 
+const mohContractAddress = '0xa7f455976455F328D6E73D345E21B319bDe6A7b3'; 
 
 const Forge = () => {
-  const [contractInstance, setContractInstance] = useState(null);
+  const [contractInstance, setContractInstance] = useState({});
   const [isOpen, setOpen] = useState(false);
   const [videoId, setVideoId] = useState('');
-  const { active } = useWeb3React();
+  const [isWeb3Installed, setIsWeb3Installed] = useState(!!(window.ethereum || window.BinanceChain));
 
   const handleConnectWallet = async () => {
-    try {
-      const provider = window.ethereum || window.BinanceChain;
-      if (provider) {
-        await provider.request({ method: 'eth_requestAccounts' });
-        const web3 = new Web3(provider);
-        const contractInstance = new web3.eth.Contract(mohContractABI, mohContractAddress);
-        setContractInstance(contractInstance);
-
-        // Add event listener for "click" event on NFT cards
-        const nftCards = document.querySelectorAll('.nft-card');
-        nftCards.forEach(card => {
-          card.addEventListener('click', () => {
-            // Get the NFT ID and name from the card
-            const nftId = card.getAttribute('data-nft-id');
-            const nftName = card.getAttribute('data-nft-name');
-          });
-        });
-
-        // handle web3 actions with the provider and contractInstance
-      } else {
-        alert('Please install Metamask or Binance Chain Wallet and connect to Binance Smart Chain');
-      }
-    } catch (error) {
-      console.error(error);
-      alert('Error connecting to wallet. Please allow access to your wallet and try again.');
+  try {
+    let provider;
+    if (window.ethereum) {
+      provider = new Web3(window.ethereum);
+    } else if (window.BinanceChain) {
+      provider = new Web3(window.BinanceChain);
+    } else {
+      alert('Please install Metamask or Binance Chain Wallet and connect to Binance Smart Chain');
+      return;
     }
+
+    const accounts = await provider.eth.requestAccounts();
+    const contractInstance = new provider.eth.Contract(mohContractABI, mohContractAddress);
+    setContractInstance(contractInstance);
+
+    // Add event listener for "click" event on NFT cards
+    const nftCards = document.querySelectorAll('.nft-card');
+    nftCards.forEach(card => {
+      card.addEventListener('click', () => {
+        // Get the NFT ID and name from the card
+        const nftId = card.getAttribute('data-nft-id');
+        const nftName = card.getAttribute('data-nft-name');
+
+        // Call the handleMint function with the NFT ID, name, and accounts
+//        handleMint(nftId, nftName, accounts);
+      });
+    });
+
+    // handle web3 actions with the provider and contractInstance
+  } catch (error) {
+    console.error(error);
+    alert('Error connecting to wallet. Please allow access to your wallet and try again.');
+    return;
   }
+}
 
   const handleMint = async (nftId, nftName, accounts) => {
   if (!contractInstance) {
@@ -149,30 +155,31 @@ const nfts = [
 
 ];
 
-turn (
-    <>
-      <div className="max-w-7xl mx-auto py-10 px-0 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 gap-20 md:grid-cols-2 lg:grid-cols-2">
-          {nfts.map((nft) => (
-            <div key={nft.id} className="border nft-card" data-nft-id={nft.id} data-nft-name={nft.name} onClick={() => handleMint(nft.id, nft.name)}>
-              <div className="relative">
-                <video className="w-full h-full object-cover" title="Click to preview NFT" src={nft.nft} autoPlay loop muted onClick={() => { setOpen(true); setVideoId(nft.videoId); }} />
-              </div>
-              <div className="p-4">
-                <h4 className="mt-0 mb-0 text-2xl text-white">{nft.name}<br/>MEDAL</h4>
-                <p className="text-sm text-white mb-0">Presented By</p>
-                <h3 className="text-sm text-white mb-12">{nft.creator}</h3>              
-                <p className="mt- text-md text-white">{nft.price}</p>
-                <button 
-                  className="mt-4 w-full inline-flex justify-center border px-4 py-2 gradient-bg-button text-base text-black hover:from-blue-400 hover:to-purple-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" 
-                  onClick={() => handleMint(nft.id, nft.name)}
-                  disabled={!active} // disable button if user doesn't have a Web3-enabled wallet
-                >
-                  {active ? 'FORGE YOUR MEDAL' : 'CONNECT WALLET'}
-                </button>
-              </div>
+  return (
+  <>
+    <div className="max-w-7xl mx-auto py-10 px-0 sm:px-6 lg:px-8">
+      <div className="grid grid-cols-1 gap-20 md:grid-cols-2 lg:grid-cols-2">
+        {nfts.map((nft) => (
+          <div key={nft.id} className="border nft-card" data-nft-id={nft.id} data-nft-name={nft.name} onClick={() => handleMint(nft.id, nft.name)}>
+            <div className="relative">
+              <video className="w-full h-full object-cover" title="Click to preview NFT" src={nft.nft} autoPlay loop muted onClick={() => { setOpen(true); setVideoId(nft.videoId); }} />
             </div>
-          ))}
+            <div className="p-4">
+              <h4 className="mt-0 mb-0 text-2xl text-white">{nft.name}<br/>MEDAL</h4>
+              <p className="text-sm text-white mb-0">Presented By</p>
+              <h3 className="text-sm text-white mb-12">{nft.creator}</h3>              
+              <p className="mt- text-md text-white">{nft.price}</p>
+            <button 
+        className="mt-4 w-full inline-flex justify-center border px-4 py-2 
+        gradient-bg-button text-base text-black hover:from-blue-400 hover:to-purple-400 focus:outline-none 
+        focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" 
+        onClick={() => handleMint(nft.id, nft.name, accounts)}
+     >
+             FORGE YOUR MEDAL
+          </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
     <ModalVideo channel='youtube' isOpen={isOpen} videoId={videoId} onClose={() => setOpen(false)} />
